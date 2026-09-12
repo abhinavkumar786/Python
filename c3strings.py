de5715ec82d2e48347afec08de4f8faf7732a7ea
abhinavkumar <abhinavkumar405353@gmail.com>
2026-09-12 23:27:04 +0530

    Updatedd c3strings.py

--- c3strings.py
+++ c3strings.py
@@ -69,3 +69,14 @@
 #     else:
 #         continue
 # print(c)
+
+
+text = input("Enter a string: ")
+
+vowels = 0
+
+for char in text:
+    if char.lower() in "aeiou":
+        vowels += 1
+
+print("Number of vowels:", vowels)
