61beafeef97c325f5e64828c4a9d7e939ca6fe2c
abhinavkumar <abhinavkumar405353@gmail.com>
2026-09-12 23:38:36 +0530

    Updated C5dictionaryANDset.py

--- C5dictionaryANDset.py
+++ C5dictionaryANDset.py
@@ -112,5 +112,18 @@ print("Total marks:", total)
 print("Average marks:", average)
 
 
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+common = set()
+
+for num in set1:
+    if num in set2:
+        common.add(num)
+
+print("Set 1:", set1)
+print("Set 2:", set2)
+print("Common elements:", common)
+
 
 
