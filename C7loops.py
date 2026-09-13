98a61c5157b51e0f59e897aa578d1b3af8544619
abhinavkumar <abhinavkumar405353@gmail.com>
2026-09-13 22:44:36 +0530

    Updated C7loops.py

--- C7loops.py
+++ C7loops.py
@@ -94,5 +94,16 @@ for i in range(0,x):
         print(c)
 
 
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
 
+common = set()
+
+for num in set1:
+    if num in set2:
+        common.add(num)
+
+print("Set 1:", set1)
+print("Set 2:", set2)
+print("Common elements:", common)
 
